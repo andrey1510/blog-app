9d67a27ef9d00c9c0ba0347ee341ff6e5840d61b
andrey1510 <andrey.ivn15@gmail.com>
2025-01-22 01:11:14 +0300

    changed Postgres to H2, changed web config

--- src/main/java/configs/DatasourceConfig.java
+++ src/main/java/configs/DatasourceConfig.java
@@ -1,11 +1,14 @@
-package configs;
+package com.blog.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,38 +17,47 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
+@EnableJpaRepositories(basePackages = "com.blog")
 @EnableTransactionManagement
-@EnableJpaRepositories
+@PropertySource("classpath:application.properties")
 public class DatasourceConfig {
+
+    private final Environment environment;
+
+    public DatasourceConfig(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("datasource.driver-class-name");
-        dataSource.setUrl("datasource.url");
-        dataSource.setUsername("datasource.username");
-        dataSource.setPassword("datasource.password");
+        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver"));
+        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+        dataSource.setPassword(environment.getProperty("spring.datasource.password"));
         return dataSource;
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
         LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
-        entityManager.setDataSource(dataSource());
+        entityManager.setDataSource(dataSource);
         entityManager.setPackagesToScan("com.blog");
         entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        Properties properties = new Properties();
-        properties.put("hibernate.dialect", properties.getProperty("hibernate.dialect"));
-        properties.put("hibernate.show_sql", properties.getProperty("hibernate.show-sql"));
-        properties.put("hibernate.hbm2ddl.auto", properties.getProperty("hibernate.hbm2ddl-auto"));
-        entityManager.setJpaProperties(properties);
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("hibernate.hbm2ddl.auto", environment.getProperty("spring.jpa.hibernate.ddl-auto"));
+        jpaProperties.put("hibernate.dialect", environment.getProperty("spring.jpa.properties.hibernate.dialect"));
+        jpaProperties.put("hibernate.show_sql", environment.getProperty("spring.jpa.show-sql"));
+        entityManager.setJpaProperties(jpaProperties);
+
         return entityManager;
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager() {
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
         return transactionManager;
     }
 }
--- src/main/java/configs/ThymeleafConfig.java
+++ src/main/java/configs/ThymeleafConfig.java
@@ -1,4 +1,4 @@
-package configs;
+package com.blog.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
