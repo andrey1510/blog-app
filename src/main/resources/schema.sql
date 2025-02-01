891c0130acccf9979a3dc6f0accf0b0ca0c76aca
andrey1510 <andrey.ivn15@gmail.com>
2025-02-01 20:59:39 +0300

    JPA changed to JDBC, changed config, added SQL scheme

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE tags (
+                      id INT PRIMARY KEY AUTO_INCREMENT,
+                      name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE posts (
+                       id INT PRIMARY KEY AUTO_INCREMENT,
+                       title VARCHAR(255) NOT NULL,
+                       text TEXT NOT NULL,
+                       image_path VARCHAR(255),
+                       likes INT DEFAULT 0
+);
+
+CREATE TABLE comments (
+                          id INT PRIMARY KEY AUTO_INCREMENT,
+                          text TEXT NOT NULL,
+                          post_id INT NOT NULL,
+                          FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
+);
+
+CREATE TABLE post_tag (
+                          post_id INT NOT NULL,
+                          tag_id INT NOT NULL,
+                          PRIMARY KEY (post_id, tag_id),
+                          FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+                          FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
+);
