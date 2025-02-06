66b5eb482b5fbb7b313f4684c4e4282c49eeb35e
andrey1510 <andrey.ivn15@gmail.com>
2025-02-06 20:09:50 +0300

    moved upload-directory to properties

--- src/main/java/com/blog/configs/WebConfiguration.java
+++ src/main/java/com/blog/configs/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.blog.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
@@ -10,6 +11,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
+    @Value("${images.upload-directory}")
+    private String uploadDirectory;
+
     @Bean
     public StandardServletMultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
@@ -17,8 +21,8 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String uploadPath = "file:" + System.getProperty("catalina.base") + "/uploads/";
-        registry.addResourceHandler("/uploads/**")
+        String uploadPath = "file:" + System.getProperty("catalina.base") + uploadDirectory;
+        registry.addResourceHandler(uploadDirectory +"**")
             .addResourceLocations(uploadPath)
             .setCachePeriod(3600);
     }
